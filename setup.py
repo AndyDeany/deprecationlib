0fcc6d3a4992e2366ab8d6cd8f1cbcf928725226
Andrew Dean <oneandydean@hotmail.com>
2017-09-19 23:12:25 +0100

    Add setup.py file for making python egg

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+import deprecationlib
+
+
+setup(
+    name=deprecationlib.__name__,
+    version=deprecationlib.__version__,
+    description=deprecationlib.__doc__,
+    url=deprecationlib.__url__,
+    author=deprecationlib.__author__,
+    author_email=deprecationlib.__author_email__,
+    packages=["deprecationlib"],
+    classifiers=(
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
+    )
+)
